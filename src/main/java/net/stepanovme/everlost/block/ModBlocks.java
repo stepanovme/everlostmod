6dec5364151f02956d3509e00f5f013959837c52
stepanovme <st.stepanov57@gmail.com>
2022-12-06 23:44:20 +0300

    Add creative blocks tab

--- src/main/java/net/stepanovme/everlost/block/ModBlocks.java
+++ src/main/java/net/stepanovme/everlost/block/ModBlocks.java
@@ -12,6 +12,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.stepanovme.everlost.Everlost;
+import net.stepanovme.everlost.item.ModCreativeModTab;
 import net.stepanovme.everlost.item.ModItem;
 
 import java.util.function.Supplier;
@@ -21,17 +22,17 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, Everlost.Mod_ID);
 
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_RIGHT = registerBlock("angular_base_of_aragis_right",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_LEFT = registerBlock("angular_base_of_aragis_left",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_BOTTOM_RIGHT = registerBlock("angular_base_of_aragis_bottom_right",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_BOTTOM_LEFT = registerBlock("angular_base_of_aragis_bottom_left",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_COLUMN = registerBlock("angular_base_of_aragis_column",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
     public static final RegistryObject<Block> ANGULAR_BASE_OF_ARAGIS_ROW = registerBlock("angular_base_of_aragis_row",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()), ModCreativeModTab.EVERLOST_BLOCKS_TAB);
 
     public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/stepanovme/everlost/item/ModCreativeModTab.java
+++ src/main/java/net/stepanovme/everlost/item/ModCreativeModTab.java
@@ -2,12 +2,20 @@ package net.stepanovme.everlost.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.stepanovme.everlost.block.ModBlocks;
 
 public class ModCreativeModTab {
     public static final CreativeModeTab EVERLOST_ITEM_TAB = new CreativeModeTab("everlostitemtab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItem.FairyDust.get());
+        }
+    };
+
+    public static final CreativeModeTab EVERLOST_BLOCKS_TAB = new CreativeModeTab("everlostblockstab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModBlocks.ANGULAR_BASE_OF_ARAGIS_COLUMN.get());
         }
     };
 }
